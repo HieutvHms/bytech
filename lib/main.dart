e9541a384b6b664717c90912fa44dca2126afbd6
Manh99 <manhvv@bsafe.vn>
2023-02-21 10:28:49 +0700

    Change font

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        fontFamily: 'Roboto',
+        fontFamily: 'Inter',
       ),
       home: const Root(),
     );
