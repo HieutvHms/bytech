14501acf1a1a1e2881aedbed623d8cc7cdb5128f
Manh99 <manhvv@bsafe.vn>
2023-02-28 15:45:06 +0700

    Fix logic auto connect

--- lib/providers/ble_provider.dart
+++ lib/providers/ble_provider.dart
@@ -1,11 +1,26 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:reintechnik/service/ble_service.dart';
+import 'package:rxdart/rxdart.dart';
+
+enum BLEStatus {
+  INITIAL,
+  CONNECTING,
+  CONNECTED,
+  DISCONNECTED,
+  ERROR,
+  ERROR_NO_DEVICES,
+}
 
 class BLEProvider extends ChangeNotifier {
   List<BluetoothDevice> bleDeviceList = [];
   BluetoothDevice? bluetoothDevice;
   BluetoothCharacteristic? bluetoothCharacteristic;
+
+  final bleStatus = BehaviorSubject<BLEStatus>()..add(BLEStatus.INITIAL);
+
   void scanDevice() async {
     bleDeviceList = await BLEService.instance.startScanDevice();
     notifyListeners();
@@ -15,18 +30,13 @@ class BLEProvider extends ChangeNotifier {
     try {
       await device.connect();
       bluetoothDevice = device;
-
-      // device.state.listen(
-      //   (event) {
-      //     if (event == BluetoothDeviceState.disconnected) {
-      //       bluetoothDevice = null;
-      //       notifyListeners();
-      //     }
-      //   },
-      // );
-      discoveryService(device);
+      device.state.listen((event) {
+        if (event == BluetoothDeviceState.disconnected) {
+          connectToDevice(device);
+        }
+      });
     } catch (e) {
-      print(e);
+      bleStatus.add(BLEStatus.ERROR);
     }
   }
 
